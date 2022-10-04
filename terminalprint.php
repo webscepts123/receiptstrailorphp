26ee56233809f85c0acdfabc4e5c46fa4720b787
webscept_solutions <accounts@webscepts.com>
2022-10-04 19:09:37 +0530

    terminal printing

--- terminalprint.php
+++ terminalprint.php
@@ -1,2 +1,20 @@
+<?php
 
+include_once("connection.php");
+
+ $sql = "SELECT * FROM rentorders WHERE id='" . $_GET["id"] . "'";
+
+    $result=mysqli_query($mysqli,$sql);
+    $singleRow = mysqli_fetch_assoc($result);
+
+require __DIR__ . '/vendor/autoload.php';   
+use Mike42\Escpos\PrintConnectors\NetworkPrintConnector;
+use Mike42\Escpos\PrintConnectors\FilePrintConnector;
+use Mike42\Escpos\Printer;
+$connector = new NetworkPrintConnector("192.168.1.23", 9600);
+$printer = new Printer($connector);
+$printer -> text("Hello World!\n");
+$printer -> cut();
+$printer -> close();
+?>
 
