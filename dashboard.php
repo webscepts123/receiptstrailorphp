7e6d9cdd13e23711aff9a6f0aa906ef958dadf53
webscept_solutions <accounts@webscepts.com>
2022-10-10 12:38:07 +0530

    some dashbaord changes

--- dashboard.php
+++ dashboard.php
@@ -139,7 +139,7 @@ $sales[] = $row['price'];
             <!-- small box -->
             <div class="small-box bg-info">
               <div class="inner">
-                <h3>150</h3>
+                <h3><?php echo "$rowspp"; ?></h3>
 
                 <p>New Orders</p>
               </div>
@@ -154,9 +154,9 @@ $sales[] = $row['price'];
             <!-- small box -->
             <div class="small-box bg-success">
               <div class="inner">
-                <h3>53<sup style="font-size: 20px">%</sup></h3>
+                <h3><?php echo "$rows"; ?></h3>
 
-                <p>Bounce Rate</p>
+                <p>Due Orders</p>
               </div>
               <div class="icon">
                 <i class="ion ion-stats-bars"></i>
@@ -169,9 +169,9 @@ $sales[] = $row['price'];
             <!-- small box -->
             <div class="small-box bg-warning">
               <div class="inner">
-                <h3>44</h3>
+                <h3><?php echo "$rowsbss"; ?></h3>
 
-                <p>User Registrations</p>
+                <p>Customers</p>
               </div>
               <div class="icon">
                 <i class="ion ion-person-add"></i>
