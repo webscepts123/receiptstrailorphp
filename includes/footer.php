b747b0765429e6a0027f2adc1177a5ab341f545b
webscept_solutions <accounts@webscepts.com>
2022-10-10 12:03:55 +0530

    new change

--- includes/footer.php
+++ includes/footer.php
@@ -8,7 +8,7 @@
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 3.2.0
     </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2022 <a href="https://adminlte.io">Shan Tailors</a>.</strong> All rights reserved.
   </footer>
 
   <!-- Control Sidebar -->
@@ -28,6 +28,5 @@
 <!-- AdminLTE App -->
 <script src="assets/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="assets/dist/js/demo.js"></script>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -190,7 +190,7 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item">
-            <a href="Dashboard" class="nav-link">
+            <a href="dashboard.php" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
