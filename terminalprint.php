3c786d1a2ea10d6faaf103fc005f9de9940b2d33
webscept_solutions <accounts@webscepts.com>
2022-10-17 18:17:37 +0530

    test

--- terminalprint.php
+++ terminalprint.php
@@ -1,56 +1,84 @@
-<?php
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Web-based raw printing example</title>
+</head>
+<body>
+<h1>Web-based raw printing example</h1>
+
+<p>This snippet forwards raw data to a local websocket.</p>
+
+<form>
+  <input type="button" onclick="directPrintBytes(printSocket, [0x1b, 0x40, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x0a, 0x1d, 0x56, 0x41, 0x03]);" value="Print test string"/>
+  <input type="button" onclick="directPrintFile(printSocket, 'receipt-with-logo.bin');" value="Load and print 'receipt-with-logo'" />
+</form>
+
+<script type="text/javascript">
 /**
- * This standalone script can be used to print the contents of a code page
- * for troubleshooting.
- *
- * Usage: php escpos-caracter-table.php NUMBER ...
- *
- * Code pages are numbered 0-255.
- *
- * The ESC/POS binary will be send to stdout, and should be redirected to a
- * file or printer:
- *
- *   php escpos-caracter-table.php 20 > /dev/usb/lp0
- *
- * @author Michael Billington < michael.billington@gmail.com >
- * @license MIT
+ * Retrieve binary data via XMLHttpRequest and print it.
  */
-
-/* Sanity check */
-if(php_sapi_name() !== "cli") {
-    die("This is a command-line script, invoke via php.exen");
-}
-if(count($argv) < 2) {
-    die("At least one code page number must be specifiedn");
-}
-array_shift($argv);
-foreach($argv as $codePage) {
-    if(!is_numeric($codePage) || $codePage < 0 || $codePage > 255) {
-        die("Code pages must be numbered 0-255");
+function directPrintFile(socket, path) {
+  // Get binary data
+  var req = new XMLHttpRequest();
+  req.open("GET", path, true);
+  req.responseType = "arraybuffer";
+  console.log("directPrintFile(): Making request for binary file");
+  req.onload = function (oEvent) {
+    console.log("directPrintFile(): Response received");
+    var arrayBuffer = req.response; // Note: not req.responseText
+    if (arrayBuffer) {
+      var result = directPrint(socket, arrayBuffer);
+      if(!result) {
+        alert('Failed, check the console for more info.');
+      }
     }
+  };
+  req.send(null);
 }
 
-/* Reset */
-$str = "\x1b@";
-foreach($argv as $codePage) {
-    /* Print header, switch code page */
-    $str .= "\x1bt" . chr($codePage);
-    $str .= "\x1bE\x01Code page $codePage\x1bE\x00\n";
-    $str .= "\x1bE\x01  0123456789ABCDEF0123456789ABCDEF\x1bE\x00\n";
-    $chars = str_repeat(' ', 128);
-    for ($i = 0; $i < 128; $i++) {
-        $chars[$i] = chr($i + 128);
-    }
-    for ($y = 0; $y < 4; $y++) {
-        $row = "" . " ";
-        $rowHeader = "\x1bE\x01" . strtoupper(dechex($y + 8)) . "\x1bE\x00";
-        $row = substr($chars, $y * 32, 32);
-        $str .= "$rowHeader $row\n";
-    }
+/**
+ * Extract binary data from a byte array print it.
+ */
+function directPrintBytes(socket, bytes) {
+  var result = directPrint(socket, new Uint8Array(bytes).buffer);
+  if(!result) {
+    alert('Failed, check the console for more info.');
+  }
 }
 
-/* Cut */
-$str .= "\x1dV\x41\x03";
+/**
+ * Send ArrayBuffer of binary data.
+ */
+function directPrint(socket, printData) {
+  // Type check
+  if (!(printData instanceof ArrayBuffer)) {
+    console.log("directPrint(): Argument type must be ArrayBuffer.")
+    return false;
+  }
+  if(printSocket.readyState !== printSocket.OPEN) {
+    console.log("directPrint(): Socket is not open!");
+    return false;
+  }
+  // Serialise, send.
+  console.log("Sending " + printData.byteLength + " bytes of print data.");
+  printSocket.send(printData);
+  return true;
+}
 
-/* Output to STDOUT */
-file_put_contents("php://stdout", $str);
+/**
+ * Connect to print server on startup.
+ */
+var printSocket = new WebSocket("ws://localhost:5555", ["binary"]);
+printSocket.binaryType = 'arraybuffer';
+printSocket.onopen = function (event) {
+  console.log("Socket is connected.");
+}
+printSocket.onerror = function(event) {
+  console.log('Socket error', event);
+};
+printSocket.onclose = function(event) {
+  console.log('Socket is closed');
+}
+</script>
+</body>
+</html>
