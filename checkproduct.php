7623a6b844e564ac5368c8e1cd96c361c24c6c41
AMILA MALMEEWALA <websceptsolution@gmail.com>
2022-12-05 10:08:35 +0530

    fixing ui

--- checkproduct.php
+++ checkproduct.php
@@ -9,85 +9,50 @@ $sql = "SELECT * FROM coats WHERE code='" . $_GET["code"] . "'";
 
 $result=mysqli_query($mysqli,$sql);
 $singleRow = mysqli_fetch_assoc($result);
-
-
-
-
 ?>
-
-                                       
-
-
-<div class="container">
-   <form action="" method="GET">
-        <label>Enter Code</label>
-        <br>
-        <br>
-        <input type="text" class="form-control"name="code" placeholder="Enter Code " required value="<?php if(isset($_GET['code'])){ echo $_GET['code']; } ?>">
-        <br>
-        <input type="submit" class="btn btn-success" value="Check">
-        <br>
-    </form>
-    
-    
-                              
-                        
                                 
-              </tbody>
-      <div class="container p-3 my-3 bg-dark text-white">
-          ID Number : <?php echo $singleRow['id']; ?>
-          <br>
-          <br>
-         
-          Product Name :<?php echo $singleRow['productname']; ?>
-          <br>
-          <br>
-      
-          <br>
-          
-      </div>
-      
-      
-      
-      <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Customer Name</th>
-      <th scope="col">Order Id</th>
-      <th scope="col">Status</th>
-      <th scope="col">Total Amount</th>
-     
-      <th scope="col">Action</th>
-
-    </tr>
-  </thead>
-  <tbody id="myUL">
-      
-      
-     
-
- 
-		 <tr>
-		 <td>$res['customername']</td>
-		 <td>$res['customername']</td>
-		 <td>$res['status']</td>
-	   <td>$res['total_amt']</td>
-		
-        <td><a class='btn btn-primary' href=\"completeorder.php?id=$res[id]\">Complete Order</a>  </td>	
-
-
-	
-	
-
- 
-  </tbody>
-</table>
-      
-      <form action="stockadd.php" method="POST">
-          <H3>Want to Add Stock</H3>
-          <input type="submit" class="btn btn-success" value="Add To Stock">
-      </form>
-
+<div class="container">
+  <br><br>
+  <div class="card p-5">
+    <form action="" method="GET">
+      <label>Enter Code</label>
+      <br><br>
+      <input type="text" class="form-control"name="code" placeholder="Enter Code " required value="<?php if(isset($_GET['code'])){ echo $_GET['code']; } ?>">
+      <br>
+      <input type="submit" class="btn btn-success" value="Check">
+      <br>
+    </form>        
+    <div class="container p-3 my-3 bg-dark text-white">
+        ID Number : <?php echo $singleRow['id']; ?>
+        <br><br>
+        Product Name :<?php echo $singleRow['productname']; ?>
+        <br><br>   
+    </div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Customer Name</th>
+          <th scope="col">Order Id</th>
+          <th scope="col">Status</th>
+          <th scope="col">Total Amount</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody id="myUL">
+    		<tr>
+      		<td>$res['customername']</td>
+      		<td>$res['customername']</td>
+      		<td>$res['status']</td>
+      	  <td>$res['total_amt']</td>
+          <td><a class='btn btn-primary' href=\"completeorder.php?id=$res[id]\">Complete Order</a>  </td>	
+        </tr>
+      </tbody>
+    </table>
+    <form action="stockadd.php" method="POST">
+        <H3>Want to Add Stock</H3>
+        <input type="submit" class="btn btn-success" value="Add To Stock">
+    </form>
+  </div>
+  <br><br>
 </div>
-
 <?php include 'includes/footer.php';?>
